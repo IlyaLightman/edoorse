0c03b45775526ff86505b90ef07efe38e101d97e
Ilya Zaytsev <ilya@zaytsev.xyz>
2020-04-29 20:19:26 +0300

    register router and mail transporter

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,21 +4,47 @@ const { hash, compare } = require('bcryptjs')
 const config = require('config')
 const jwt = require('jsonwebtoken')
 const { registerValidators } = require('../utils/validators')
+const User = require('../models/User')
+const nodemailer = require('nodemailer')
+const sendgrid = require('nodemailer-sendgrid-transport')
 
 const router = Router()
 
+const transporter = nodemailer.createTransport(sendgrid({
+	auth: { api_key: config.get('SENDGRID_API_KEY') }
+}))
+
 // /api/auth/register
-router.post('/register', registerValidators, async (req, res) => {
-	try {
-		const { email, password, name } = req.body
+router.post('/register', registerValidators,
+	async (req, res) => {
+		try {
+			const errors = validationResult(req)
 
-		const errors = validationResult(req)
-		if (!errors.isEmpty()) {
-			// Ошибка валидации
+			if (!errors.isEmpty()) {
+				return res.status(400).json({
+					errors: errors.array(),
+					message: 'Некорректные данные при регистрации'
+				})
+			}
+
+			const { email, password, name } = req.body
+
+			const candidate = await User.findOne({ email })
+			if (candidate) {
+				res.status(400).json({
+					message: 'Пользователь с таким Email уже существует'
+				})
+			}
+
+			const hashPassword = await hash(password, 10)
+			const user = new User({ email, password: hashPassword, name })
+
+			await user.save()
+
+			res.status(201).json({
+				message: `Пользователь ${name} успешно создан`
+			})
+		} catch (err) {
+			console.log(err)
 		}
-
-		// TODO логика регистрации
-	} catch (err) {
-		console.log(err)
-	}
 })
