9f290df13a635208e68421d988e2b8b79f58b4e3
Ilya Zaytsev <ilya@zaytsev.xyz>
2020-05-02 16:57:12 +0300

    redux

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker'
+
+import { compose, createStore, applyMiddleware } from 'redux'
+import rootReducer from './store/reducers/rootReducer'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+
+const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        }) : compose;
+
+const store = createStore(
+    rootReducer,
+    composeEnhancers(
+        applyMiddleware(thunk)
+    )
+)
+
+const app = (
+    <Provider store={store}>
+
+        <App />
+
+    </Provider>
+)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      {app}
   </React.StrictMode>,
   document.getElementById('root')
 );
