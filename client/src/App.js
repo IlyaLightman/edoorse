70514270602a7e0c077552dbf6ec6f443bc057ca
Ilya Zaytsev <ilya@zaytsev.xyz>
2020-05-13 12:19:21 +0300

    switch routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch, Redirect, withRouter } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Edoorse</h1>
-          <button
-            onClick={
-                async () => {
-                    await fetch('api/auth/dbtest', {
-                        method: 'POST',
-                        headers: {},
-                        body: null
-                    })
-                }
-            }
-          >DB Test</button>
-      </header>
-    </div>
+      <Switch>
+          <Route path='/' component={
+              <div className="App">
+                  <header className="App-header">
+                      <h1>Edoorse</h1>
+                      <button
+                          onClick={
+                              async () => {
+                                  await fetch('api/auth/dbtest', {
+                                      method: 'POST',
+                                      headers: {},
+                                      body: null
+                                  })
+                              }
+                          }
+                      >DB Test</button>
+                  </header>
+              </div>
+          } />
+          <Route path='/login' component={} />
+      </Switch>
   );
 }
 
