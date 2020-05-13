7f6c792dd7cfe35264d41cdcab9c80843b28a87c
Ilya Zaytsev <ilya@zaytsev.xyz>
2020-05-13 12:14:43 +0300

    http react hook

---
+++ client/src/hooks/http.hook.js
@@ -0,0 +1,41 @@
+import { useState, useCallback } from 'react'
+
+const useHttp = () => {
+	const [ loading, setLoading ] = useState(false)
+	const [ error, setError ] = useState(null)
+
+	const request = useCallback(async (url, method = 'GET', body = null, headers = {}) => {
+		setLoading(true)
+		try {
+			if (body) {
+				body = JSON.stringify(body)
+				headers['Content-Type'] = 'application/json'
+			}
+
+			const response = await fetch(url, {
+				method, body, headers
+			})
+			const data = await response.json()
+
+			if (!response.ok) {
+				console.log('всё очень плохо, а может и не очень', data)
+				setError(data.errors|| 'Что-то не так...')
+				// throw new Error(data.message || 'Something wrong')
+			}
+
+			setLoading(false)
+
+			return data
+		} catch (err) {
+			setLoading(false)
+			setError(err.message)
+			// throw err
+		}
+	}, [])
+
+	const clearError = useCallback(() => setError(null), [])
+
+	return { loading, request, error, clearError }
+}
+
+export default useHttp
